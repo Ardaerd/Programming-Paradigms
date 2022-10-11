07caa6459ee074d6fd93030255622c6bd92bef9c
Ardaerd <arderd@outlook.com>
2022-10-11 17:10:37 +0300

    Lec_1: pointer exercise_1

---
+++ Lecture_1/build-lec_1-Desktop-Debug/CMakeFiles/lec_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lec_1.dir/main.cpp.o"
+  "CMakeFiles/lec_1.dir/main.cpp.o.d"
+  "lec_1"
+  "lec_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lec_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
