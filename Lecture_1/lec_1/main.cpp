fc01dc00a15ba5c1873f59821056bd29164923cb
Ardaerd <arderd@outlook.com>
2022-10-11 17:57:39 +0300

    Lec_2: struct exercise_1

--- Lecture_1/lec_1/main.cpp
+++ Lecture_1/lec_1/main.cpp
@@ -4,9 +4,21 @@ using namespace std;
 
 int main()
 {
-    string* a = new string();
-    *a = "Hello World!";
+    // Struct is a type like int
+    struct {int a; int b;} A;
+    A.a = 1;
+    A.b = 5;
 
-    cout << a << endl;
+    struct {int a; int b;} B;
+    B.a = -1;
+    B.b = 8;
+
+    struct {int a; int b;} C;
+    C.a = A.a + B.a;
+    C.b = A.b + B.b;
+
+    cout << "(" << C.a << "," << C.b << ")" << endl;
+
+
     return 0;
 }
