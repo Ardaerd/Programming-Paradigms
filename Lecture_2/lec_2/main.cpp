9cc73f0b9da83d3db97ca7144bd8e192c3710811
Ardaerd <arderd@outlook.com>
2022-10-12 13:58:37 +0300

    lec_2: typedef exercise_2

---
+++ Lecture_2/lec_2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+// It is a global definiation of the Vector2d
+typedef struct { int a; int b; } Vector2d;
+
+Vector2d addVectors(Vector2d K, Vector2d L) {
+    Vector2d M;
+
+    M.a = K.a + L.a;
+    M.b = K.b + L.b;
+
+    return M;
+}
+
+int main()
+{
+    Vector2d A;
+    A.a = 1;
+    A.b = 5;
+
+    Vector2d B;
+    B.a = -1;
+    B.b = 8;
+
+    Vector2d C = addVectors(A, B);
+
+    cout << "(" << C.a << "," << C.b << ")" << endl;
+
+    return 0;
+}
