bce61fb777f5205701ae2c6785bd8c0059c37a01
Ardaerd <arderd@outlook.com>
2022-10-12 18:27:44 +0300

    reamde: pointer vs reference first part done

--- Lecture_2/lec_2/main.cpp
+++ Lecture_2/lec_2/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 // It is a global definiation of the Vector2d
 typedef struct { int a; int b; } Vector2d;
 
-Vector2d addVectors(Vector2d K, Vector2d L) {
+Vector2d addVectors(Vector2d* K, Vector2d* L) {
     Vector2d M;
 
-    M.a = K.a + L.a;
-    M.b = K.b + L.b;
+    M.a = K->a + L->a;
+    M.b = K->b + L->b;
 
     return M;
 }
@@ -24,7 +24,8 @@ int main()
     B.a = -1;
     B.b = 8;
 
-    Vector2d C = addVectors(A, B);
+    // It is not copiying objects any more. Now we are giving the reference of the these objects
+    Vector2d C = addVectors(&A, &B);
 
     cout << "(" << C.a << "," << C.b << ")" << endl;
 
