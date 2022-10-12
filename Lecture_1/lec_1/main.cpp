c88d9cdd3d4d2f2f1387b783a75f4b2a6e2d9cc3
Ardaerd <arderd@outlook.com>
2022-10-12 13:17:32 +0300

    lec_2: typedef exercise_1

--- Lecture_1/lec_1/main.cpp
+++ Lecture_1/lec_1/main.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 int main()
 {
-    // Struct is a type like int
-    struct {int a; int b;} A;
+    // It is not a instance, it is a new type
+    typedef struct {int a; int b;} Vector2d;
+
+    Vector2d A;
     A.a = 1;
     A.b = 5;
 
-    struct {int a; int b;} B;
+    Vector2d B;
     B.a = -1;
     B.b = 8;
 
-    struct {int a; int b;} C;
+    Vector2d C;
     C.a = A.a + B.a;
     C.b = A.b + B.b;
 
     cout << "(" << C.a << "," << C.b << ")" << endl;
-
 
     return 0;
 }
