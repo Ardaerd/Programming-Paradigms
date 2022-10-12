d91bf3371550c0219c85084f80364d3ab1b0e929
Ardaerd <arderd@outlook.com>
2022-10-12 18:39:19 +0300

    lec_2: references exercise

--- Lecture_2/lec_2/main.cpp
+++ Lecture_2/lec_2/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 // It is a global definiation of the Vector2d
 typedef struct { int a; int b; } Vector2d;
 
-Vector2d addVectors(Vector2d* K, Vector2d* L) {
+Vector2d addVectors(Vector2d& K, Vector2d& L) {
     Vector2d M;
 
-    M.a = K->a + L->a;
-    M.b = K->b + L->b;
+    M.a = K.a + L.a;
+    M.b = K.b + L.b;
 
     return M;
 }
@@ -25,7 +25,7 @@ int main()
     B.b = 8;
 
     // It is not copiying objects any more. Now we are giving the reference of the these objects
-    Vector2d C = addVectors(&A, &B);
+    Vector2d C = addVectors(A, B);
 
     cout << "(" << C.a << "," << C.b << ")" << endl;
 
