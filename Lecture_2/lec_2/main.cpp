98ec6645a8c81018b093e387a10c6e5622851809
Ardaerd <arderd@outlook.com>
2022-10-22 16:28:25 +0300

    Lec_2: printVector and constructVector functions

--- Lecture_2/lec_2/main.cpp
+++ Lecture_2/lec_2/main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 // It is a global definiation of the Vector2d
-typedef struct { int a; int b; } Vector2d;
+// typedef struct { int a; int b; } Vector2d;
+
+// It is the same thing as the definiation above. But in a simpler way
+struct Vector2d {
+  int a;
+  int b;
+};
 
 // taking the references of the objects. In reference, we can use it like object but can't change the values.
 Vector2d addVectors(Vector2d& K, Vector2d& L) {
@@ -15,18 +21,33 @@ Vector2d addVectors(Vector2d& K, Vector2d& L) {
     return M;
 }
 
+
+// For constructing the contents of the vector
+void constructVector2d(Vector2d& v, int a, int b) {
+    v.a = a;
+    v.b = b;
+}
+
+// For printing the content of the vector
+void printVector(Vector2d& v) {
+    std::cout << "Content of the Vector: " << "(" << v.a << "," << v.b << ")" << std::endl;
+}
+
+
 int main()
 {
     Vector2d A;
-    A.a = 1;
-    A.b = 5;
+    constructVector2d(A,1,5);
+    printVector(A);
+
 
     Vector2d B;
-    B.a = -1;
-    B.b = 8;
+    constructVector2d(B,-1,8);
+    printVector(B);
 
     // Giving function the objects
     Vector2d C = addVectors(A, B);
+    printVector(C);
 
     cout << "(" << C.a << "," << C.b << ")" << endl;
 
