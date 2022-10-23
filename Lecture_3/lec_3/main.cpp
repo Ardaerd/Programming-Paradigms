28f8ba1d7116e4163e6c1aa4f502d2a75fd26392
Ardaerd <arderd@outlook.com>
2022-10-23 19:06:12 +0300

    Lec_3: Content is added

--- Lecture_3/lec_3/main.cpp
+++ Lecture_3/lec_3/main.cpp
@@ -1,4 +1,29 @@
+// C has malloc and free for dynamic memory handling.
+// C++ has new/delete.
+// static variables, static member variables
+// Generic Programming = Templates in C++
+
+// const keyword
+
+// OOP:
+// ctor/dtor
+// copy-ctor, copy-assignment
+// inheritance (later)
+
+// function overloading
+// operators and operator overloading
+// function parameters: pass by value, pass by reference
+
+// next week:
+// function pointers
+// for_each
+
+// auto, AAA rule (almost always auto)
+
 #include <iostream>
+
+
+
 
 using namespace std;
 
