57e8415ef89f3f37c6d0d37279131295c5e112bb
Ardaerd <arderd@outlook.com>
2022-10-23 19:05:45 +0300

    Lec_3: Content is added

---
+++ Lecture_3/build-lec_3-Desktop-Debug/CMakeFiles/lec_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lec_3.dir/main.cpp.o"
+  "CMakeFiles/lec_3.dir/main.cpp.o.d"
+  "lec_3"
+  "lec_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lec_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
