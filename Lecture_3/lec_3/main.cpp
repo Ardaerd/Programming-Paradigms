6d42bf31ebc227fd47373db5853e1345de8b7208
Ardaerd <arderd@outlook.com>
2022-10-24 15:00:31 +0300

    update

--- Lecture_3/lec_3/main.cpp
+++ Lecture_3/lec_3/main.cpp
@@ -92,6 +92,10 @@ struct VectorInt {
 
 int main()
 {
+    int b = 5;
+    int* a = &b;
+    std::cout << *a << std::endl;
+
     int v1_values[2] = {10,20};
     VectorInt v1 = VectorInt(2, v1_values);
     v1.print();
@@ -105,6 +109,7 @@ int main()
     // In this case this declaration couldn't compile because operator+ is no longer a free function
     //std::cout << operator+(v1,v2) << std::endl;
 
+    // It is automatically executed, even though it was in the OOP
     bool is_okay = v1 + v2;
 
     if (is_okay) {
