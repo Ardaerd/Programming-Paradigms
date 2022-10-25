561e910f3d557085dcee743dbbe31cc3b0e63f79
Ardaerd <arderd@outlook.com>
2022-10-25 21:06:14 +0300

    Lec_4: basic implementation is done

---
+++ Lecture_4/build-lec_4-Desktop-Debug/CMakeFiles/lec_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lec_4.dir/main.cpp.o"
+  "CMakeFiles/lec_4.dir/main.cpp.o.d"
+  "lec_4"
+  "lec_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lec_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lecture_4/lec_4/main.cpp
@@ -0,0 +1,77 @@
+// function pointers
+// for_each
+
+// Fraction class: Want to represent rational numbers with a numerator and denominator
+
+// OOP: (encapsulates all necessary tools in a box that communicates
+//  with outside via c-tors, assignment operators, and casting
+// ctor/dtor
+// copy-ctor, copy-assignment
+// casting
+// inheritance (will discuss later)
+
+// function overloading, overload set
+// operators and operator overloading
+
+// literals, types of constants (remember units in your Physics class: 9.8 m/s^2)
+// user-defined literals (a special way to create your own literals)
+
+// Celsius/Fahrenheit example
+// converting constructors
+
+// pass by value, pass by reference, pass by constant reference
+// implicit/explicit constructors
+
+// operator== and operator+ overloads for Celsius/Fahrenheit
+// static attributes and staticness. static variable initialization
+
+#include <iostream>
+
+using namespace std;
+
+
+int dosmt1(int a, int b) { return a; }
+int dosmt2(int a, int b) { return a + b; }
+int dosmt3(int a, int b) { return a - b; }
+
+int main()
+{
+
+    int choice = 0;
+    int retval = 0;
+
+    if (choice == 0)
+        retval = dosmt1(1,2);
+
+    else if (choice == 1)
+        retval = dosmt2(1,2);
+
+    else if (choice == 2)
+        retval = dosmt3(1,2);
+
+    cout << retval << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
