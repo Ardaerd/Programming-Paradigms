94fec4e7c60f56f369f2966b9c3dffe03e0b1831
Ardaerd <arderd@outlook.com>
2022-12-31 15:00:43 +0300

    Lec_8: Lambada functions are implemented

--- Lecture_8/build-lec_8-Desktop-Debug/CMakeFiles/lec_8.dir/compiler_depend.make
+++ Lecture_8/build-lec_8-Desktop-Debug/CMakeFiles/lec_8.dir/compiler_depend.make
@@ -192,8 +192,21 @@ CMakeFiles/lec_8.dir/main.cpp.o: /home/arda/Cs_321/Lecture_8/lec_8/main.cpp \
   /usr/include/c++/11/bits/basic_ios.tcc \
   /usr/include/c++/11/bits/ostream.tcc \
   /usr/include/c++/11/istream \
-  /usr/include/c++/11/bits/istream.tcc
+  /usr/include/c++/11/bits/istream.tcc \
+  /usr/include/c++/11/vector \
+  /usr/include/c++/11/bits/stl_uninitialized.h \
+  /usr/include/c++/11/bits/stl_vector.h \
+  /usr/include/c++/11/bits/stl_bvector.h \
+  /usr/include/c++/11/bits/vector.tcc
 
+
+/usr/include/c++/11/bits/vector.tcc:
+
+/usr/include/c++/11/bits/stl_bvector.h:
+
+/usr/include/c++/11/bits/stl_uninitialized.h:
+
+/usr/include/c++/11/vector:
 
 /usr/include/c++/11/bits/istream.tcc:
 
@@ -218,6 +231,8 @@ CMakeFiles/lec_8.dir/main.cpp.o: /home/arda/Cs_321/Lecture_8/lec_8/main.cpp \
 /usr/include/c++/11/stdexcept:
 
 /usr/include/x86_64-linux-gnu/c++/11/bits/error_constants.h:
+
+/usr/include/c++/11/bits/stl_vector.h:
 
 /usr/include/c++/11/system_error:
 
--- Lecture_8/lec_8/main.cpp
+++ Lecture_8/lec_8/main.cpp
@@ -16,8 +16,40 @@ struct Painting {
     string Painter;
 };
 
+bool greater_than(int i, int j) { return i > j; }
+bool greater_than_1900(int i) { return i > 1900; }
+
+auto greater_than_lambda(int i) {
+    return [i](int j) {
+        return j > i;
+    };
+}
+
+// using Fucntion object
+struct GreaterThan {
+    int i;
+    bool operator() (int j) { return i > j; }
+};
+
+// this is returning a lambda which calculates a business logic
+auto greaterThanLambda = [](int i, int j) { return i > j; };
+auto greaterThan1900_Lambda = [i = 1900](int j) { return i > j; };
+
+
+auto gt = GreaterThan{1900};
+auto isGreater = gt(2000);
+
 int main()
 {
+    auto gt_l = greater_than_lambda(10);
+    cout << gt_l(41) << endl;
+    auto gtl = greaterThanLambda(10,20);
+    auto gt1900_L = greaterThan1900_Lambda(200);
+
+    cout << isGreater << endl;
+    cout << gtl << endl;
+    cout << gt1900_L << endl;
+
 
     auto p =  Painting{Color::Red,1950,"Picasso"};
     bool isGreater = p.year > 1900;
@@ -28,6 +60,18 @@ int main()
       {Color::Red,1950,"Picasso"},
       {Color::Yellow,1975,"Dali"},
       {Color::Blue,1530,"Da Vinci"},
+    };
+
+    auto v_name = vector<string>{
+        "Picasso",
+        "Dali",
+        "Monet"
+    };
+
+    auto v_year = vector<int>{
+        1950,
+        1975,
+        1530
     };
 
     return 0;
